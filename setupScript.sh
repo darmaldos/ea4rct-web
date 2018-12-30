323e30314b223232182f2f4d31b87f1548974fc6
Fran Acien <amil101ftw@gmail.com>
2018-12-30 13:28:36 +0100

    adding bash script for updating repositoy and building with hugo
    Former-commit-id: 420a3d590428d051f989a8fb8cf9f133cb138c63

---
+++ setupScript.sh
@@ -0,0 +1,11 @@
+# This is a simple script for update hugo web page hosted on gitea
+# Due to the bad documentation of Gitea and Drone, this is a partial solution
+
+BASE_DIR="/home/amil101/gitRepos/ea4rct-web/"
+
+logger "[ea4rct-web] Updating git repository on $BASE_DIR"
+cd $BASE_DIR
+git pull
+
+logger "[ea4rct-web] Building web page"
+hugo
